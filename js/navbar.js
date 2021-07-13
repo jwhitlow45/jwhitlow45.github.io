fa8e477bed5a92cc39ed1445657e29f8056c2678
jdubbleu <jwhitlow45@gmail.com>
2021-07-13 13:28:42 -0400

    feat:add function to dynamically load pages with jquery

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,33 @@
 // Global elements
-hamburger_menu = document.getElementById("navbar-burger")
-navbar_menu = document.getElementById("navbar-menu")
+const hamburger_menu = document.getElementById("navbar-burger");
+const navbar_menu = document.getElementById("navbar-menu");
+// navbar buttons
+const navbar_home = document.getElementById("home");
+const navbar_education = document.getElementById("education");
+const navbar_experience = document.getElementById("experience");
+const navbar_projects = document.getElementById("projects");
+const navbar_references = document.getElementById("references");
+// page content
+const page_content = document.getElementById("content");
+
 // Event listeners
 hamburger_menu.addEventListener("click", toggleHamburgerMenu);
+navbar_home.addEventListener("click", changePageContent);
+navbar_education.addEventListener("click", changePageContent);
+navbar_experience.addEventListener("click", changePageContent);
+navbar_projects.addEventListener("click", changePageContent);
+navbar_references.addEventListener("click", changePageContent);
 
 function toggleHamburgerMenu() {
   hamburger_menu.classList.toggle("is-active");
   navbar_menu.classList.toggle("is-active");
-  console.log("here")
+}
+
+function clearPageContent() {
+  page_content.innerHTML = "";
+}
+
+function changePageContent(event) {
+  clearPageContent();
+  $(page_content).load("./html/" + event.target.id + ".html");
 }
